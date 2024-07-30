02f3eb1d6af027ab17ac4bd28548094684797607
pwledger <108314068+pwledger@users.noreply.github.com>
2024-07-30 14:39:10 +0900

    자바스크립트 가위바위보 게임

---
+++ 가위바위보_자바스크립트/script.js
@@ -0,0 +1,13 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const choices = ['rock', 'paper', 'scissors'];
+
+
+    document.querySelectorAll('.choice').forEach(button => {
+
+    });
+
+    function determineWinner(player, computer) {
+
+    }
+});
